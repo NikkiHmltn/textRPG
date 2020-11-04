50b0c75822e4aa45c3dca23da2d485dd8df7001c
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-11-04 14:04:27 -0800

    [js] add working reset button, fixed button displays, fixed scenario

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ let gameArea = document.querySelector(".game-area");
 let playerDisplay = document.querySelector(".player-info");
 let buttonArea = document.querySelector(".button-container");
 let infoArea = document.querySelector(".info-container")
-
+let playerInfo = document.querySelector(".player-display")
+let currentMonster 
 
 
 // only one class for now, default class
 let fighterClass = {
     health: 80,
     strength: 3,
+    stamina: 40,
     ability: {
         id: 0,
         name: "Slice and Dice",
@@ -25,7 +27,7 @@ let player = {
     name: "TBD",
     health: fighterClass.health,
     strength: fighterClass.strength,
-    stamina: 40,
+    stamina: fighterClass.stamina,
     gold: 0,
     checkHealth() {
         if (player.health <= 0) {
@@ -38,7 +40,7 @@ let player = {
 //fight mechanics go here
 function pickMonster() {
     randomMonster = monster[Math.floor(Math.random() * monster.length)]
-    console.log(randomMonster);
+    return randomMonster
 }
 
 function fightTime() {
@@ -57,6 +59,10 @@ fightTime(pickMonster())
 const displayInstructions = () => {
     gameArea.textContent = 'Click to explore, fight monsters, and save your head from the chopping block! \n Press Start Game to begin your journey.' 
 }
+playerInfo.innerText = `Name: ${player.name} \n Health: ${player.health}\n Strength: ${player.strength} \n Stamina: ${player.stamina} \n Gold: ${player.gold}`;
+
+    playerInfo.style.textAlign = "right";
+
 
 //on click it starts the game
 const startGame = () => {
@@ -73,34 +79,40 @@ const startGame = () => {
     yesBtn.id = "yes-btn";
     buttonArea.appendChild(yesBtn);
 
-    let clicked = false;
     yesBtn.addEventListener('click', function(){
-        clicked = true;
         scenario1();
     });
     noBtn.addEventListener('click', function(){
-        clicked = true;
         gameOver1();
     });
-
-    playerDisplay.innerText = `Name: ${player.name} \n Health: ${player.health}\n Strength: ${player.strength} \n Stamina: ${player.stamina} \n Gold: ${player.gold}`;
-
-    playerDisplay.style.textAlign = "right";
+    
+    startButton.style.display = 'none';
 
     //creates a reset button once start game has been clicked
     let resetBtn = document.createElement('button');
     resetBtn.textContent = 'Reset'
     resetBtn.id = 'reset-btn'
+    resetBtn.style.display = 'block'
     playerDisplay.appendChild(resetBtn);
     resetBtn.addEventListener('click', function(){
-        clicked = true;
-        gameArea.textContent = " ";
+        infoArea.textContent = " ";
+        buttonArea.textContent = " ";
+    
+        if (startButton.style.display === 'none') {
+            (startButton.style.display = 'block')
+        }
+        if (resetBtn.style.display === 'block') {
+            (resetBtn.style.display = 'none')
+        }
         
+        //remove hidden properties
     })
 };
 
+//give these buttons a hidden class
 instructions.addEventListener('click', displayInstructions);
-startButton.addEventListener('click', startGame);
+//hide start on click and reveal reset button
+startButton.addEventListener('click', startGame)
 
 
 
--- scenarios.js
+++ scenarios.js
@@ -3,19 +3,39 @@
 function scenario1 () {
     infoArea.textContent = "You hear the click of the door unlocking as the king and his guards greet you at the door. The king explains his daughter, the princess, has fallen ill. He wants you to retrieve a potion from the hermit in the swamp that may be able to save her. He gives you a golden card and instructs you to keep it until after the task is completed. It looks worth a lot. Despite being recently freed, the king swiftly kicks you out of the city and instructs you to get on your way. Good thing the guards gave you back your old weapons.";
 
+    let btns = document.querySelector('.button-container').children;
+    
+    while (btns.length) {
+        btns[0].remove();
+    }
+    
     let adventureBtn = document.createElement('button');
     adventureBtn.id = 'travel-btn';
     adventureBtn.textContent = 'Venture Forth!'
     buttonArea.appendChild(adventureBtn)
 
-    let btns = document.querySelector('button-container');
-    if (btns.hasChildNodes()) {
-        btns.removeChild(btns.childNodes[0]);
-        btns.removeChild(btns.childNodes[1]);
-    }
-
+    adventureBtn.addEventListener('click', function() {
+        scenario2();
+    })
 
 };
+
+function scenario2() {
+    infoArea.textContent = "test for scenario 2"
+
+    let btns = document.querySelector('.button-container').children;
+    
+    while (btns.length) {
+        btns[0].remove();
+    }
+
+    let battleBtn = document.createElement('button');
+    battleBtn.id = 'battle-btn';
+    battleBtn.textContent = 'Fight!'
+    buttonArea.appendChild(battleBtn)
+
+
+}
 
 //These functions are lose conditions written into the story
 
